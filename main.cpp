4bba6b6d901e371b418f0aafcb46364a0518caa9
markcty <markcty@outlook.com>
2020-12-12 15:36:31 +0800

    change font to give a better view in Windows

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
 #include <savegame.h>
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    QFont font("Arial");
+    app.setFont(font);
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
